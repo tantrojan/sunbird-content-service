f3d8e3cf8bc60151fc7f2ca959850f520900ccf5
tantrojan <tg.99ghosh@gmail.com>
2018-06-21 18:03:12 +0530

    All containers name fetched

--- src/service/iitbx.js
+++ src/service/iitbx.js
@@ -1,7 +1,22 @@
+/*
+Made by Tanmoy Ghosh (tantrojan)
+*/
+const request =require("request");
+
 module.exports = {
 	
 	getAllAPI : function(req,res){
-		
+			var serverIp = "http://10.196.0.56:8000/";
+			var requestUrl = "files/info/"
+			var requestParameter = {
+				uri: serverIp + requestUrl,
+				method: "GET",
+			}
+
+			request(requestParameter, function(error, response, body) {
+			  res.send(response.body);
+			  // res.send(body);
+			});
 	},
 	getByNameAPI : function(req,res){
 
