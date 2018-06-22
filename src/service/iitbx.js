e8e247ff8a69b1b5c72b7d29693f6768a8131629
tantrojan <tg.99ghosh@gmail.com>
2018-06-22 12:11:04 +0530

    List of containers returned

--- src/service/iitbx.js
+++ src/service/iitbx.js
@@ -2,21 +2,61 @@
 Made by Tanmoy Ghosh (tantrojan)
 */
 const request =require("request");
+var storageUrl="";
+var xAuth="";
+
+function generateSwiftAuth()
+{
+	var serverIp = "http://10.129.103.86:5000/";
+	var requestUrl = "v3/auth/tokens"
+	var requestParameter = {
+		uri: serverIp + requestUrl,
+		method: "POST",
+		body : '\n{ "auth": {\n    "identity": {\n      "methods": ["password"],\n      "password": {\n        "user": {\n          "name": "swift",\n          "domain": { "name": "default" },\n          "password": "swift"\n        }\n      }\n    },\n    "scope": {\n      "project": {\n        "name": "service",\n        "domain": { "name": "default" }\n      }\n    }\n  }\n}'
+	}
+
+	request(requestParameter, function(error, response, body) {
+		// res.send();
+		var resobj=JSON.parse(response.body)
+		xAuth=response.headers['x-subject-token'];
+		// xAuth=xAuth['x-subject-token'];
+		storageUrl=resobj.token.catalog[0].endpoints[2].url;
+		storageUrl=storageUrl.replace("controller","10.129.103.86")
+		// res.send("storageUrl :"+ storageUrl + "\n xAuth :"+ xAuth);
+	});
+}
+
+generateSwiftAuth();
 
 module.exports = {
 	
 	getAllAPI : function(req,res){
-			var serverIp = "http://10.196.0.56:8000/";
-			var requestUrl = "files/info/"
-			var requestParameter = {
-				uri: serverIp + requestUrl,
-				method: "GET",
+		
+		console.log(storageUrl +"\n"+ xAuth);
+		var requestParameter = {
+			uri: storageUrl,
+			method: "GET",
+			headers : {
+				'x-auth-token' : xAuth,
+				'content-type' : 'application/json'
+				// 'data-type'	: 'image/png'
 			}
+		}
 
-			request(requestParameter, function(error, response, body) {
-			  res.send(response.body);
-			  // res.send(body);
-			});
+		var matrix={};
+		request(requestParameter, function(error, response, body) {
+		  var containers = response.body.split("\n");
+		  containers=containers.slice(0,-1);
+		  console.log(containers);
+		  for(var i=0;i<containers.length;i++)
+		  {
+		  	
+		  	matrix[i]=containers[i];
+		  }
+		  res.send(matrix);
+		  // // res.send(body);
+		  // res.send(response);
+		});
 	},
 	getByNameAPI : function(req,res){
 
